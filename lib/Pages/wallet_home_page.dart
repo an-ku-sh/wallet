3e6a3b93a6fe99e13612171c26896c7a78d2acad
an-ku-sh <110815021+an-ku-sh@users.noreply.github.com>
2023-11-09 18:50:15 +0530

    decided to shift to web3dart functions

--- lib/Pages/wallet_home_page.dart
+++ lib/Pages/wallet_home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wallet/Pages/onboarding.dart';
-import 'package:wallet/Utils/wallet_provider.dart';
 import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart';
+import '../Utils/constants.dart';
 
 class Wallet extends StatefulWidget {
   const Wallet({super.key});
@@ -12,12 +13,19 @@ class Wallet extends StatefulWidget {
 }
 
 class WalletState extends State<Wallet> {
+  //Wallet Credentials
   String walletAddress = '';
-  String balance = '';
+  String? balance = '';
   String pvKey = '';
+
+  //Web3
+  Client? httpClient;
+  Web3Client? ethClient;
 
   @override
   void initState() {
+    httpClient = Client();
+    ethClient = Web3Client(infura_url, httpClient!);
     super.initState();
     loadWalletData();
   }
@@ -26,24 +34,41 @@ class WalletState extends State<Wallet> {
     print("load wallet called");
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? privateKey = prefs.getString('privateKey');
-    if (privateKey != null) {
-      final walletProvider = WalletProvider();
-      await walletProvider.loadPrivateKey();
-      EthereumAddress address = await walletProvider.getPublicKey(privateKey);
-      print(address.hex);
-      setState(() {
-        walletAddress = address.hex;
-        pvKey = privateKey;
-      });
-      print(pvKey);
-    }
+    //fetching balance using default web3dart methods
+    final credentials = EthPrivateKey.fromHex(privateKey!);
+    final address = credentials.address;
+    print(address.hexEip55);
+    print(await ethClient?.getBalance(address));
+
+    await ethClient?.sendTransaction(
+      credentials,
+      Transaction(
+        to: EthereumAddress.fromHex(
+          '0x4B92A586C2010a539EE5f9261dD1Ba4D1c1f43AD',
+        ),
+        gasPrice: EtherAmount.inWei(BigInt.one),
+        maxGas: 100000,
+        value: EtherAmount.fromUnitAndValue(EtherUnit.wei, 10000000000000000),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(walletAddress),
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  // sendEther('0x4B92A586C2010a539EE5f9261dD1Ba4D1c1f43AD',
+                  //     '0.02', ethClient!, pvKey);
+                },
+                child: Text("SendEth")),
+            Text(walletAddress),
+            Text(balance!)
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: () async {
         SharedPreferences prefs = await SharedPreferences.getInstance();
---
+++ lib/Utils/constants.dart
@@ -0,0 +1,4 @@
+String infura_url =
+    "https://goerli.infura.io/v3/541d9e3028d64feb81ebebdb5b2edc3e";
+
+String contract_address = "0xA426FE72f337fA07cC9FF071c93937C2d64F9A31";
